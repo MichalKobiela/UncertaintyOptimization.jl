46499b72bf3ecd0ec62f029a6b9d75ebc60d81b7
Documenter.jl <documenter@juliadocs.github.io>
2025-10-21 14:26:56 +0000

    build based on 2397027

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#UncertaintyOptimization","page":"Home","title":"UncertaintyOptimization","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for UncertaintyOptimization.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
