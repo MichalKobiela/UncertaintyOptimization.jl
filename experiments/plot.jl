3dbf5a416455410b8a7a06e28906680325edebbf
bieniekmateusz <bieniekmat@gmail.com>
2026-04-09 11:04:35 +0100

    plotter script

---
+++ experiments/plot.jl
@@ -0,0 +1,48 @@
+using Revise
+using UncertaintyOptimization
+using ModelingToolkit
+using ModelingToolkit: t_nounits as t, D_nounits as D;
+using OrdinaryDiffEq
+using CSV, Tables
+using Turing
+using SciMLBase: VectorOfArray
+using SymbolicIndexingInterface
+using Random
+using PreallocationTools
+using Serialization
+using CSV, Tables
+using Plots
+
+
+#open the chains
+chain = open(string(@__DIR__)*"/posterior_samples_large_range_1_c11_r1_u0AD.jls", "r") do io
+        deserialize(io)
+end
+
+# the right order - mat
+# 0 - sigma
+# 1:nx2, 2:beta_1, 3:beta_2, 4:alpha_4, 
+# 5:nr, 6:r2, 7:r1, 8:nx1, 9:beta_3, 
+# 10:kr, 11:kx1, 12:alpha_2, 13:alpha_1, 
+# 14:alpha_3, 15:beta_4
+
+chain_2 = open(string(@__DIR__)*"/minmtk_c1.jls", "r") do io
+        deserialize(io)
+end
+
+# chain_3 = open(string(@__DIR__)*"/posterior_samples_large_range_1_c_r5.jls", "r") do io
+#         deserialize(io)
+# end
+
+StatsPlots.plot(chain)
+StatsPlots.plot!(chain_2)
+# StatsPlots.plot!(chain_3)
+
+# Increased burnout for better mixing
+# StatsPlots.plot(chain[1:1000])
+# StatsPlots.plot!(chain_2[1:1000])
+# StatsPlots.plot!(chain_3[1000:2000])
+
+
+# save the plot
+savefig(string(@__DIR__)*"/posterior.pdf")
